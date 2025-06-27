66912676d4344e441f1df351455fca9cc949c5c3
Mayank <mayank0evil0side@gmail.com>
2025-06-27 17:45:09 +0530

    Merged

--- website/website/urls.py
+++ website/website/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False)),  # ← redirect root to /admin/
-    # path('schema-viewer/', include('schema_viewer.urls')),
+    path('schema-viewer/', include('schema_viewer.urls')),
     path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include("authentication.urls")),
