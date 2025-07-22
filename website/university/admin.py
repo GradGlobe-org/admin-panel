df333b5f666ec9797b789e7f9ebd4fb877511af1
Aaryan Kumar <aaryanaks3@gmail.com>
2025-07-23 00:49:50 +0530

    added bulk status change option

--- website/university/admin.py
+++ website/university/admin.py
@@ -1,67 +1,63 @@
 from django.contrib import admin
 from .models import (
-    university,
-    location,
-    stats,
-    videos_links,
-    ranking_agency,
-    university_ranking,
-    faqs,
-    Uni_contact,
-    commission,
-    Partner_Agency,
-    mou,
-    Country,
-    WhyStudyInSection,
-    CostOfLiving,
-    AdmissionStats,
-    Visa,
-    WorkOpportunity,
+    location, Country, university, WhyStudyInSection, CostOfLiving,
+    AdmissionStats, Visa, WorkOpportunity, Partner_Agency, commission,
+    mou, Uni_contact, stats, videos_links, ranking_agency,
+    university_ranking, faqs
 )
 
-# === INLINES ===
+# === INLINE MODELS FOR UNIVERSITY ===
 
 class StatsInline(admin.TabularInline):
     model = stats
     extra = 1
 
+
 class VideosInline(admin.TabularInline):
     model = videos_links
     extra = 1
+
 
 class FaqsInline(admin.TabularInline):
     model = faqs
     extra = 1
 
+
 class UniversityRankingInline(admin.TabularInline):
     model = university_ranking
     extra = 1
+
 
 class UniContactInline(admin.TabularInline):
     model = Uni_contact
     extra = 1
 
+
 class CommissionInline(admin.TabularInline):
     model = commission
     extra = 1
+
 
 class MouInline(admin.TabularInline):
     model = mou
     extra = 1
 
+
 class AdmissionStatsInline(admin.TabularInline):
     model = AdmissionStats
     extra = 1
+
 
 class VisaInline(admin.TabularInline):
     model = Visa
     extra = 1
 
+
 class WorkOpportunityInline(admin.TabularInline):
     model = WorkOpportunity
     extra = 1
 
-# === MODEL ADMINS ===
+# === UNIVERSITY ADMIN ===
 
 @admin.register(university)
 class UniversityAdmin(admin.ModelAdmin):
@@ -70,113 +66,83 @@ class UniversityAdmin(admin.ModelAdmin):
     search_fields = ('name', 'location__city', 'location__state', 'location__country')
     raw_id_fields = ('location',)
     ordering = ('name',)
-    fields = (
-        'cover_url', 'cover_origin', 'name', 'type', 'establish_year', 'location',
-        'about', 'admission_requirements', 'location_map_link', 'avg_acceptance_rate',
-        'avg_tution_fee', 'review_rating', 'status'
+    actions = ['make_published', 'make_draft']
+
+    fieldsets = (
+        ('Basic Info', {
+            'fields': ('cover_url', 'cover_origin', 'name', 'type', 'establish_year', 'location', 'status')
+        }),
+        ('Academic & Admission Info', {
+            'fields': ('about', 'admission_requirements')
+        }),
+        ('Other Details', {
+            'fields': ('location_map_link', 'avg_acceptance_rate', 'avg_tution_fee', 'review_rating')
+        }),
     )
+
     inlines = [
-        StatsInline, VideosInline, FaqsInline, UniversityRankingInline,
-        UniContactInline, CommissionInline, MouInline,
-        AdmissionStatsInline, VisaInline, WorkOpportunityInline
+        StatsInline,
+        VideosInline,
+        FaqsInline,
+        UniversityRankingInline,
+        UniContactInline,
+        CommissionInline,
+        MouInline,
+        AdmissionStatsInline,
+        VisaInline,
+        WorkOpportunityInline,
     ]
 
     def get_country(self, obj):
         return obj.location.country
     get_country.short_description = 'Country'
 
-@admin.register(location)
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'city', 'state', 'country')
-    search_fields = ('city', 'state', 'country')
+    @admin.action(description="Mark selected universities as Published")
+    def make_published(self, request, queryset):
+        queryset.update(status='PUBLISH')
 
-@admin.register(stats)
-class StatsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'value', 'university')
-    search_fields = ('name', 'value', 'university__name')
-    raw_id_fields = ('university',)
+    @admin.action(description="Mark selected universities as Draft")
+    def make_draft(self, request, queryset):
+        queryset.update(status='DRAFT')
 
-@admin.register(videos_links)
-class VideosLinksAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url', 'university')
-    search_fields = ('url', 'university__name')
-    raw_id_fields = ('university',)
 
-@admin.register(ranking_agency)
-class RankingAgencyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'logo')
-    search_fields = ('name',)
-
-@admin.register(university_ranking)
-class UniversityRankingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'university', 'ranking_agency', 'rank')
-    list_filter = ('ranking_agency',)
-    search_fields = ('university__name', 'ranking_agency__name')
-    raw_id_fields = ('university', 'ranking_agency')
-
-@admin.register(faqs)
-class FaqsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question', 'university')
-    search_fields = ('question', 'university__name')
-    raw_id_fields = ('university',)
-
-@admin.register(Uni_contact)
-class UniContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'designation', 'email', 'phone', 'university')
-    search_fields = ('name', 'email', 'phone', 'university__name')
-    raw_id_fields = ('university',)
-
-@admin.register(commission)
-class CommissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'inPercentage', 'inAmount', 'partner_agency', 'university')
-    search_fields = ('partner_agency__name', 'university__name')
-    raw_id_fields = ('partner_agency', 'university')
-
-@admin.register(Partner_Agency)
-class PartnerAgencyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'partner_type')
-    list_filter = ('partner_type',)
-    search_fields = ('name',)
-
-@admin.register(mou)
-class MouAdmin(admin.ModelAdmin):
-    list_display = ('id', 'MoU_copy_link', 'SigningDate', 'ExpiryDate', 'Duration_in_years', 'Duration_in_Months', 'university')
-    search_fields = ('MoU_copy_link', 'university__name')
-    list_filter = ('SigningDate', 'ExpiryDate')
-    raw_id_fields = ('university',)
+# === OTHER MODELS ===
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+    list_display = ('name',)
+
+
+@admin.register(location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('city', 'state', 'country')
+    search_fields = ('city', 'state', 'country')
+
 
 @admin.register(WhyStudyInSection)
 class WhyStudyInSectionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'country', 'content')
-    search_fields = ('country__name',)
-    raw_id_fields = ('country',)
+    list_display = ('country',)
+    search_fields = ('country__name', 'content')
+
 
 @admin.register(CostOfLiving)
 class CostOfLivingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'country', 'total_min', 'total_max')
+    list_display = ('country', 'total_min', 'total_max')
     search_fields = ('country__name',)
-    raw_id_fields = ('country',)
 
-@admin.register(AdmissionStats)
-class AdmissionStatsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'university', 'admission_type', 'GPA_min', 'GPA_max')
-    search_fields = ('university__name',)
-    raw_id_fields = ('university',)
 
-@admin.register(Visa)
-class VisaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'type_of_visa', 'cost', 'university')
-    search_fields = ('name', 'type_of_visa', 'university__name')
-    raw_id_fields = ('university',)
-    list_filter = ('type_of_visa',)
+@admin.register(Partner_Agency)
+class PartnerAgencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'partner_type')
+    search_fields = ('name',)
 
-@admin.register(WorkOpportunity)
-class WorkOpportunityAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'university')
-    search_fields = ('name', 'university__name')
-    raw_id_fields = ('university',)
+
+@admin.register(ranking_agency)
+class RankingAgencyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'description')
+
+
+# Do not register: stats, videos_links, university_ranking, faqs, etc. separately
+# because they are already inlined into the university admin
+
