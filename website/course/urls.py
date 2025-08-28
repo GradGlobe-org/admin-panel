bcc4e27375048bacec16cedeb5a54e4c4d65439e
Mayank <mayank0evil0side@gmail.com>
2025-08-28 20:36:53 +0530

    Added an Api for Filter search

--- website/course/urls.py
+++ website/course/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import search_course, compare_course_search
-
+from .views import search_course, compare_course_search, filter_search
 urlpatterns = [
 
     # Perfect No Need For Optimization
     path('search-course/', search_course, name='search-course'),
-    path('compare_course_search/', compare_course_search)
-    
+    path('compare_course_search/', compare_course_search),
+    path('filter_get/', filter_search)
 ]
