2e8afc41c84416a86f045c97dea544d05b5185d9
Aaryan Kumar <aaryanaks3@gmail.com>
2025-08-31 19:51:38 +0530

    added instagram posts support

--- website/seo/admin.py
+++ website/seo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import InstaEmbed
 
-# Register your models here.
+
+@admin.register(InstaEmbed)
+class InstaEmbedAdmin(admin.ModelAdmin):
+    list_display = ("id", "short_preview", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("embed_text",)
+
+    def short_preview(self, obj):
+        """Show a short snippet of the embed text."""
+        return obj.embed_text[:80] + ("..." if len(obj.embed_text) > 80 else "")
+    short_preview.short_description = "Embed Preview"
--- website/seo/urls.py
+++ website/seo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import meta_keywords, extract_keywords
+from .views import meta_keywords, extract_keywords, get_active_instagram_embeds
 
 
 urlpatterns = [
     path("meta_keywords/", meta_keywords),
-    path("extract-keywords/", extract_keywords)
+    path("extract-keywords/", extract_keywords),
+    path("instagram-posts/", get_active_instagram_embeds, name="instagram-posts"),
 ]
