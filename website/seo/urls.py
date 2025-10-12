563730908b8a3a8c00570f318832297ea048c36f
mayankkumargupta1 <mayank0evil0side@gmail.com>
2025-10-12 19:48:05 +0530

    Added Seo Api

--- website/seo/urls.py
+++ website/seo/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import meta_keywords, extract_keywords, get_active_instagram_embeds
+from .views import (
+    meta_keywords,
+    extract_keywords,
+    get_active_instagram_embeds,
+    get_seo_sitemap_data,
+)
 
 
 urlpatterns = [
     path("meta_keywords/", meta_keywords),
     path("extract-keywords/", extract_keywords),
     path("instagram-posts/", get_active_instagram_embeds, name="instagram-posts"),
+    path("get_seo_data/", get_seo_sitemap_data),
 ]
+
--- website/seo/views.py
+++ website/seo/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -19,16 +20,17 @@ def get_yake_keywords(text, ratio=0.04, max_keywords=20):
         lan="en",
         n=3,
         dedupLim=0.9,
-        dedupFunc='seqm',
+        dedupFunc="seqm",
         windowsSize=1,
         top=top_keywords,
-        features=None
+        features=None,
     )
 
     keywords = kw_extractor.extract_keywords(text)
     keyword_list = [kw for kw, _ in keywords]
     keyword_string = ", ".join(keyword_list)
     return keyword_string
+
 
 @token_required
 @api_key_required
@@ -52,30 +54,32 @@ def meta_keywords(request):
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
 
+
 @token_required
 @api_key_required
 @csrf_exempt
 def extract_keywords(request):
-    if request.method != 'POST':
-        return JsonResponse({'error': 'Only POST method allowed.'}, status=405)
+    if request.method != "POST":
+        return JsonResponse({"error": "Only POST method allowed."}, status=405)
 
     try:
         data = json.loads(request.body)
-        text = data.get('text')
+        text = data.get("text")
 
         if not text:
-            return JsonResponse({'error': 'No text provided.'}, status=400)
+            return JsonResponse({"error": "No text provided."}, status=400)
 
         keywords = get_yake_keywords(text)
-        return JsonResponse({'keywords': keywords})
+        return JsonResponse({"keywords": keywords})
 
     except json.JSONDecodeError:
-        return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+        return JsonResponse({"error": "Invalid JSON."}, status=400)
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        return JsonResponse({"error": str(e)}, status=500)
+
 
 @api_key_required
-@require_http_methods(['GET'])
+@require_http_methods(["GET"])
 def get_active_instagram_embeds(request):
     """
     Return all active Instagram embeds with count.
@@ -86,3 +90,18 @@ def get_active_instagram_embeds(request):
         "results": list(embeds),
     }
     return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+@api_key_required
+def get_seo_sitemap_data(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("select public.get_universities_with_courses_for_seo();")
+            row = cursor.fetchone()
+            if row and row[0]:
+                return JsonResponse({"data": row[0]}, safe=False, status=200)
+            else:
+                return JsonResponse({"data": []}, safe=False, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
