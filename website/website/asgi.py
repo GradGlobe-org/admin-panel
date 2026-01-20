1b9cd963abbf0cb15418933bc523fbda289a618f
mayankkumargupta1 <mayankcryptobyte@gmail.com>
2026-01-20 23:21:20 +0530

    ignored csrf

--- website/website/asgi.py
+++ website/website/asgi.py
@@ -3,6 +3,7 @@ import django
 from django.core.asgi import get_asgi_application
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.views.decorators.csrf import csrf_exempt
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
@@ -13,7 +14,7 @@ from strawberry.channels import GraphQLWSConsumer
 from .GlobalSchema import schema
 
 websocket_urlpatterns = [
-    path("graphql/", GraphQLWSConsumer.as_asgi(schema=schema)),
+    path("graphql/", csrf_exempt(GraphQLWSConsumer.as_asgi(schema=schema))),
 ]
 
 application = ProtocolTypeRouter({
